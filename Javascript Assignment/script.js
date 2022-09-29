a4068da8948cb20e83da5956392672055d7f6d7e
Jawwad Rafique Ahmed <55888087+JawwadRaf98@users.noreply.github.com>
2022-09-29 10:35:41 +0500

    q1 done and files created

---
+++ Javascript Assignment/script.js
@@ -0,0 +1,8 @@
+// Question no 2
+
+// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+var myName = "Jawwad Rafique Ahmed";
+// alert('Hello '+ myName + " would you like to learn JavaScript today.")
+// another way
+alert(`Hello  ${myName}   would you like to learn JavaScript today.`)
