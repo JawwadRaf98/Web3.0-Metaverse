268e6804212d164cce3c997f66ad2318e2c5a961
Jawwad Rafique Ahmed <55888087+JawwadRaf98@users.noreply.github.com>
2022-09-30 19:36:58 +0500

    dsdsa

--- Javascript Assignment/script.js
+++ Javascript Assignment/script.js
@@ -6,3 +6,22 @@ var myName = "Jawwad Rafique Ahmed";
 // alert('Hello '+ myName + " would you like to learn JavaScript today.")
 // another way
 alert(`Hello  ${myName}   would you like to learn JavaScript today.`)
+
+const toCapiltalize = (str) =>{
+    tempStr = str.split(" ");
+    tempName = ""
+    tempStr.map((item, index)=>{
+        tempName += item[0].toCapiltalize + str.slice(1).toLowerCase+ " "
+    })
+    return tempName;
+
+}
+
+var personName = "jaWwad rAfIque ahmeD";
+// Question no 3
+
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+// alert('Hello '+ myName + " would you like to learn JavaScript today.")
+// another way
+alert("Lower Case  "+personName.toLowerCase() +" , Upper Case "+personName.toUpperCase() +" , Capitalize Case "+toCapiltalize(personName) +".")
