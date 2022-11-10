81a3102ad5b8f1f49203f2947f26e5737bc70ce7
Jawwad Rafique Ahmed <55888087+JawwadRaf98@users.noreply.github.com>
2022-11-10 14:37:19 +0500

    chapter 13  practice

---
+++ Javascript Assignment/app.js
@@ -0,0 +1,50 @@
+// Chapter no 13 practice exercises
+
+// Create a function named greet() that takes one argument, fullName .
+// That argument should be an array. Output the items of the array into
+// the console interpolated into a greeting message string.
+
+const greetingName = (x) =>{
+    console.log(`Hello "${x}" welcome to our website.`)
+}
+
+const greet = (fn, cb = "") => {
+    fn.map((item, index)=>{
+        if(cb !== "") {
+        return cb(item)
+        }
+        else{
+            return console.log(`Hello "${item}" welcome to our website.`)
+        }
+    })
+}
+greet(['Irtiza','Hammad', 'Saad', 'Jawwad','Samrat'])
+greet(['Irtiza','Hammad', 'Saad', 'Jawwad','Samrat'], (x)=> console.log(`Hello "${x}" welcome to our website.`))
+
+
+let promise = (state)=>{
+
+    return(
+ new Promise((resolve,reject)=>{
+    if (state){
+        return resolve("success")
+    }else{
+        return reject("reject")
+    }
+})
+    )
+}
+
+promise(true)
+.then((value)=>{
+    console.log(value)
+    return promise(false)
+})
+.then((value)=>{
+    console.log(value)
+    return promise(false)
+})
+.catch((value)=>{
+    console.log(value)
+    console.log("Loop ended")
+})
