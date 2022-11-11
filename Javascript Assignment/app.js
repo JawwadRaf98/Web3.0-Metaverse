5a879b5123d1b4efad10d3c29d497bc66a0c6fe0
Jawwad Rafique Ahmed <55888087+JawwadRaf98@users.noreply.github.com>
2022-11-11 11:01:03 +0500

    practice

--- Javascript Assignment/app.js
+++ Javascript Assignment/app.js
@@ -1,50 +1,72 @@
-// Chapter no 13 practice exercises
+// // Chapter no 13 practice exercises
 
-// Create a function named greet() that takes one argument, fullName .
-// That argument should be an array. Output the items of the array into
-// the console interpolated into a greeting message string.
+// // Create a function named greet() that takes one argument, fullName .
+// // That argument should be an array. Output the items of the array into
+// // the console interpolated into a greeting message string.
 
-const greetingName = (x) =>{
-    console.log(`Hello "${x}" welcome to our website.`)
-}
+// const greetingName = (x) =>{
+//     console.log(`Hello "${x}" welcome to our website.`)
+// }
 
-const greet = (fn, cb = "") => {
-    fn.map((item, index)=>{
-        if(cb !== "") {
-        return cb(item)
-        }
-        else{
-            return console.log(`Hello "${item}" welcome to our website.`)
+// const greet = (fn, cb = "") => {
+//     fn.map((item, index)=>{
+//         if(cb !== "") {
+//         return cb(item)
+//         }
+//         else{
+//             return console.log(`Hello "${item}" welcome to our website.`)
+//         }
+//     })
+// }
+// greet(['Irtiza','Hammad', 'Saad', 'Jawwad','Samrat'])
+// greet(['Irtiza','Hammad', 'Saad', 'Jawwad','Samrat'], (x)=> console.log(`Hello "${x}" welcome to our website.`))
+
+
+// let promise = (state)=>{
+
+//     return(
+//  new Promise((resolve,reject)=>{
+//     if (state){
+//         return resolve("success")
+//     }else{
+//         return reject("reject")
+//     }
+// })
+//     )
+// }
+
+// promise(true)
+// .then((value)=>{
+//     console.log(value)
+//     return promise(false)
+// })
+// .then((value)=>{
+//     console.log(value)
+//     return promise(false)
+// })
+// .catch((value)=>{
+//     console.log(value)
+//     console.log("Loop ended")
+// })
+
+
+const makeJuice = new Promise((res, rej)=>{
+        if(mood>5){
+            console.log("Ok")
+            res(
+               setTimeout(()=>{
+                console.log("Your juice is  ready")
+               }, 1000)
+           )
+        }else{
+            rej(
+                console.log("sorry mood is not ready")
+            )
         }
     })
-}
-greet(['Irtiza','Hammad', 'Saad', 'Jawwad','Samrat'])
-greet(['Irtiza','Hammad', 'Saad', 'Jawwad','Samrat'], (x)=> console.log(`Hello "${x}" welcome to our website.`))
 
-
-let promise = (state)=>{
-
-    return(
- new Promise((resolve,reject)=>{
-    if (state){
-        return resolve("success")
-    }else{
-        return reject("reject")
-    }
-})
-    )
-}
-
-promise(true)
-.then((value)=>{
-    console.log(value)
-    return promise(false)
-})
-.then((value)=>{
-    console.log(value)
-    return promise(false)
-})
-.catch((value)=>{
-    console.log(value)
-    console.log("Loop ended")
+makeJuice.then(()=>{
+    console.log("resolve")
+}).catch(()=>{
+    console.log("rejecct")
 })
